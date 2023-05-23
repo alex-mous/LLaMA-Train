dc2bbb4e3190c0b4e34846bb02ae79c11b07f801
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-23 13:34:18 -0700

    added tokenizer tests

--- src/test/llama/test_tokenizer.py
+++ src/test/llama/test_tokenizer.py
@@ -5,12 +5,11 @@ Test input tokenization
 import os
 import unittest
 from src.main.llama.tokenizer import Tokenizer
-from src.main.util.data import load_pile, get_data_loader
 
 
 tokenizer_path: str = os.path.join(
     os.path.dirname(__file__).removesuffix(os.path.normpath("src/test/llama")),
-    os.path.normpath("artifacts/")
+    os.path.normpath("artifacts/base_100000.model")
 )
 
 
@@ -18,12 +17,14 @@ class TestTokenizer(unittest.TestCase):
     """
     Test tokenization from llama/tokenizer
     """
-    def test_load_pile(self):
+    def test_basic_tokenizer(self):
         """
         Check basic input tokenization.
         """
-        # tokenizer = Tokenizer(tokenizer_path)
-        pass
+        tokenizer = Tokenizer(tokenizer_path)
+        self.assertTrue(len(tokenizer.encode("in", False, False)) == 1)
+        self.assertTrue(len(tokenizer.encode("in", True, False)) == 2)
+        self.assertTrue(len(tokenizer.encode("in", True, True)) == 3)
 
 
 if __name__ == '__main__':
