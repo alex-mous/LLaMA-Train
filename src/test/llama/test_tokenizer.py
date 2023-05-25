94f1d95a7344f9862c0d5ff4b82291b6ca4ba9eb
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-25 12:01:09 -0700

    removed unused libraries

--- src/test/llama/test_tokenizer.py
+++ src/test/llama/test_tokenizer.py
@@ -2,15 +2,8 @@
 Test input tokenization
 """
 
-import os
 import unittest
 from src.main.llama.tokenizer import Tokenizer
-
-
-tokenizer_path: str = os.path.join(
-    os.path.dirname(__file__).removesuffix(os.path.normpath("src/test/llama")),
-    os.path.normpath("artifacts/base_100000.model")
-)
 
 
 class TestTokenizer(unittest.TestCase):
