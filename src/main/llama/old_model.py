ed88b6f5f2c89df1f1b51dda2acb8267466e605e
Alex Mous <amous@uw.edu>
2023-05-26 15:40:17 -0700

    Update old model to be functional on single CPU

--- src/main/llama/old_model.py
+++ src/main/llama/old_model.py
@@ -9,19 +9,14 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-import fairscale.nn.model_parallel.initialize as fs_init
-from fairscale.nn.model_parallel.layers import (
-    ParallelEmbedding,
-    RowParallelLinear,
-    ColumnParallelLinear,
-)
+from xformers.components.positional_embedding import RotaryEmbedding
 
 
 @dataclass
 class ModelArgs:
-    dim: int = 512
-    n_layers: int = 8
-    n_heads: int = 8
+    dim: int = 64  # 512
+    n_layers: int = 2  # 8
+    n_heads: int = 2  # 8
     vocab_size: int = -1  # defined later by tokenizer
     multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
     norm_eps: float = 1e-5
@@ -43,88 +38,53 @@ class RMSNorm(nn.Module):
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
 
-
-def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
-    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
-    t = torch.arange(end, device=freqs.device)  # type: ignore
-    freqs = torch.outer(t, freqs).float()  # type: ignore
-    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
-    return freqs_cis
-
-
-def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
-    ndim = x.ndim
-    assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
-    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
-    return freqs_cis.view(*shape)
-
-
 def apply_rotary_emb(
     xq: torch.Tensor,
-    xk: torch.Tensor,
-    freqs_cis: torch.Tensor,
+    xk: torch.Tensor
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
-    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
-    return xq_out.type_as(xq), xk_out.type_as(xk)
+    #xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    #xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_.type_as(xq), xk_.type_as(xk)
 
 
 class Attention(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
 
-        self.n_local_heads = args.n_heads // fs_init.get_model_parallel_world_size()
+        self.n_heads = args.n_heads
         self.head_dim = args.dim // args.n_heads
 
-        self.wq = ColumnParallelLinear(
+        self.in_proj = nn.Linear(
             args.dim,
-            args.n_heads * self.head_dim,
-            bias=False,
-            gather_output=False,
-            init_method=lambda x: x,
+            3 * args.n_heads * self.head_dim,
+            bias=False
         )
-        self.wk = ColumnParallelLinear(
-            args.dim,
-            args.n_heads * self.head_dim,
-            bias=False,
-            gather_output=False,
-            init_method=lambda x: x,
-        )
-        self.wv = ColumnParallelLinear(
-            args.dim,
-            args.n_heads * self.head_dim,
-            bias=False,
-            gather_output=False,
-            init_method=lambda x: x,
-        )
-        self.wo = RowParallelLinear(
+        self.out_proj = nn.Linear(
             args.n_heads * self.head_dim,
             args.dim,
-            bias=False,
-            input_is_parallel=True,
-            init_method=lambda x: x,
+            bias=False
         )
 
         self.cache_k = torch.zeros(
-            (args.max_batch_size, args.max_seq_len, self.n_local_heads, self.head_dim)
-        ).cuda()
+            (args.max_batch_size, args.max_seq_len, self.n_heads, self.head_dim)
+        )
         self.cache_v = torch.zeros(
-            (args.max_batch_size, args.max_seq_len, self.n_local_heads, self.head_dim)
-        ).cuda()
+            (args.max_batch_size, args.max_seq_len, self.n_heads, self.head_dim)
+        )
 
-    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
+        self.pos_embed = RotaryEmbedding(self.head_dim)
+
+    def forward(self, x: torch.Tensor, start_pos: int, mask: Optional[torch.Tensor]):
         bsz, seqlen, _ = x.shape
-        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        xq, xk, xv = self.in_proj(x).chunk(3, dim=-1)
 
-        xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-        xk = xk.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-        xv = xv.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+        xq = xq.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_heads, self.head_dim)
 
-        xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
+        xq, xk = self.pos_embed(xq, xk)
 
         self.cache_k = self.cache_k.to(xq)
         self.cache_v = self.cache_v.to(xq)
@@ -147,7 +107,7 @@ class Attention(nn.Module):
             1, 2
         ).contiguous().view(bsz, seqlen, -1)
 
-        return self.wo(output)
+        return self.out_proj(output)
 
 
 class FeedForward(nn.Module):
@@ -161,7 +121,7 @@ class FeedForward(nn.Module):
         hidden_dim = int(2 * hidden_dim / 3)
         hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
 
-        self.w1 = ColumnParallelLinear(
+        """self.w1 = ColumnParallelLinear(
             dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
         )
         self.w2 = RowParallelLinear(
@@ -169,6 +129,21 @@ class FeedForward(nn.Module):
         )
         self.w3 = ColumnParallelLinear(
             dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
+        )"""
+        self.w1 = nn.Linear(
+            in_features=dim,
+            out_features=hidden_dim,
+            bias=False
+        )
+        self.w2 = nn.Linear(
+            in_features=hidden_dim,
+            out_features=dim,
+            bias=False
+        )
+        self.w3 = nn.Linear(
+            in_features=dim,
+            out_features=hidden_dim,
+            bias=False
         )
 
     def forward(self, x):
@@ -189,8 +164,8 @@ class TransformerBlock(nn.Module):
         self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
         self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
 
-    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
-        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_cis, mask)
+    def forward(self, x: torch.Tensor, start_pos: int, mask: Optional[torch.Tensor]):
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, mask)
         out = h + self.feed_forward.forward(self.ffn_norm(h))
         return out
 
@@ -202,8 +177,8 @@ class Transformer(nn.Module):
         self.vocab_size = params.vocab_size
         self.n_layers = params.n_layers
 
-        self.tok_embeddings = ParallelEmbedding(
-            params.vocab_size, params.dim, init_method=lambda x: x
+        self.tok_embeddings = nn.Embedding(
+            params.vocab_size, params.dim
         )
 
         self.layers = torch.nn.ModuleList()
@@ -211,19 +186,13 @@ class Transformer(nn.Module):
             self.layers.append(TransformerBlock(layer_id, params))
 
         self.norm = RMSNorm(params.dim, eps=params.norm_eps)
-        self.output = ColumnParallelLinear(
-            params.dim, params.vocab_size, bias=False, init_method=lambda x: x
-        )
-
-        self.freqs_cis = precompute_freqs_cis(
-            self.params.dim // self.params.n_heads, self.params.max_seq_len * 2
+        self.output = nn.Linear(
+            params.dim, params.vocab_size, bias=False
         )
 
     def forward(self, tokens: torch.Tensor, start_pos: int):
         _bsz, seqlen = tokens.shape
         h = self.tok_embeddings(tokens)
-        self.freqs_cis = self.freqs_cis.to(h.device)
-        freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
 
         mask = None
         if seqlen > 1:
@@ -231,7 +200,7 @@ class Transformer(nn.Module):
             mask = torch.triu(mask, diagonal=start_pos + 1).type_as(h)
 
         for layer in self.layers:
-            h = layer(h, start_pos, freqs_cis, mask)
+            h = layer(h, start_pos, mask)
         h = self.norm(h)
         output = self.output(h[:, -1, :])  # only compute last logits
         return output.float()
--- src/test/llama/test_model.py
+++ src/test/llama/test_model.py
@@ -3,6 +3,9 @@ Test default transformer model
 """
 
 import unittest
+
+import torch
+
 from src.main.llama.old_model import ModelArgs, Transformer
 from src.main.llama.tokenizer import Tokenizer
 
@@ -11,12 +14,44 @@ class TestTransformer(unittest.TestCase):
     """
     Test model in llama/model.
     """
-    def test_basic_model(self):
+    def test_old_model(self):
         """
-
+        Tests basic functionality (constructors and processing several examples) in old_model
         """
         args = ModelArgs
+        tokenizer = Tokenizer()
+        args.vocab_size = tokenizer.n_words
         model = Transformer(args)
+        inputs = ["the cat sat on the mat", "the cats sit on the mats", "the cat sats on the mat"]
+        input_tokens = [tokenizer.encode(input) for input in inputs]
+        min_prompt_size = min(len(t) for t in input_tokens)  # min token length
+        max_prompt_size = max(len(t) for t in input_tokens)
+        total_len = 25   # generation lengths
+        tokens = torch.full((len(inputs), total_len), -1).long()  # batch size x max possible len
+        for k, t in enumerate(input_tokens):
+            tokens[k, : len(t)] = torch.tensor(t).long()  # fill in prompts
+        input_text_mask = tokens != -1  # mask out padding
+        start_pos = min_prompt_size
+        prev_pos = 0
+        # iterate over first non-generated token in batch to predict next token for all prompts in batch
+        for cur_pos in range(start_pos, total_len):
+            logits = model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            next_token = torch.argmax(logits, dim=-1).reshape(-1)
+
+            # replace only if we don't already have this token in our promt
+            next_token = torch.where(
+                input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
+            )
+            tokens[:, cur_pos] = next_token
+            prev_pos = cur_pos
+
+        # detokenize resulting generations to string
+        decoded = []
+        for i, t in enumerate(tokens.tolist()):
+            # cut to max gen len
+            t = t[: len(input_tokens[i]) + total_len]
+            decoded.append(tokenizer.decode(t))
+        print(decoded)
 
 
 if __name__ == '__main__':
