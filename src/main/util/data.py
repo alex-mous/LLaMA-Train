6fe7a61797c39f264276f66ac30bad5b8a03d32d
Alex Mous <amous@uw.edu>
2023-05-30 22:40:51 -0700

    Update data loading to parse exact sequence length samples

--- src/main/util/data.py
+++ src/main/util/data.py
@@ -43,19 +43,24 @@ def process_file(datafile: str, max_seqs: int = 20000, seq_len: int = 2048) -> t
     :return: Tensor of dimension (up to max_seqs, seq_len)
     """
     seqs = torch.zeros((1, seq_len))
+
     tokenizer = Tokenizer()
     with open(datafile, "r", encoding="utf-8") as file:
-        for jsonline in file:
-            print(seqs.shape)
-            if seqs.shape[0]-1 >= max_seqs:
-                break
-            line = json.loads(jsonline)
-            tokens = torch.tensor(tokenizer.encode(line["text"]))
-            print(tokens)
-            print(len(tokens) % seq_len)
-            print(tokens[:-(len(tokens) % seq_len)])
-            tokens = torch.reshape(tokens[:-(len(tokens) % seq_len)], (-1, seq_len))
-            torch.vstack((seqs, tokens))
+        with tqdm(total=max_seqs) as p_bar:
+            curr_tokens = torch.tensor([])
+            for jsonline in file:
+                if seqs.shape[0] > max_seqs:
+                    break
+                line = json.loads(jsonline)
+                new_tokens = torch.tensor(tokenizer.encode(line["text"]))
+                # print(len(tokens))
+                curr_tokens = torch.cat((curr_tokens, new_tokens))
+                if len(curr_tokens) > seq_len:
+                    tokens = torch.reshape(curr_tokens[:-(len(curr_tokens) % seq_len)], (-1, seq_len))
+                    curr_tokens = curr_tokens[tokens.shape[0]*seq_len : ]
+                    # print(tokens.shape, seqs.shape)
+                    seqs = torch.vstack((seqs, tokens))
+                    p_bar.update(1)
     return seqs[1:]
 
 
@@ -69,7 +74,7 @@ def load_pile(train_size: int = 20000, val_size: int = 10000, test_size: int = 0
     :param seq_len:
     :return: train, test, val PileDatasets
     """
-    train_toks = process_file(os.path.join(data_path, "07.jsonl"), train_size, seq_len)
+    train_toks = process_file(os.path.join(data_path, "train.jsonl"), train_size, seq_len)
     val_toks = process_file(os.path.join(data_path, "val.jsonl"), val_size, seq_len)
     test_toks = process_file(os.path.join(data_path, "test.jsonl"), test_size, seq_len)
     train = PileDataset(train_toks)
