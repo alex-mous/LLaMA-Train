e6afda00c0f517c84adbe29a4ffef40ebd9dd750
Alex Mous <amous@uw.edu>
2023-06-03 14:08:11 -0700

    Some changes after running model on Colab, and add notebook

--- src/main/llama/llama.py
+++ src/main/llama/llama.py
@@ -8,16 +8,13 @@ from src.main.llama import Transformer, XFormersTransformer, Tokenizer, ModelArg
 
 def load_llama(
         tokenizer_path: str,
-        checkpoint_dir: Optional[str],
         initial_checkpoint: Optional[str],
         use_xformers: bool = False,
         **model_args
 ) -> Tuple[nn.Module, Tokenizer]:
     # Load LLaMa model and tokenizer with given parameters
-    # TODO: checkpoint loading
     start_time = time.time()
     print("Loading LLaMa model and tokenizer")
-    # checkpoint = torch.load(ckpt_path, map_location="cpu")
     tokenizer = Tokenizer(model_path=tokenizer_path)
     model_params = ModelArgs(**model_args)
     model_params.vocab_size = tokenizer.n_words
@@ -26,5 +23,7 @@ def load_llama(
     else:
         model = Transformer(model_params)
     torch.set_default_tensor_type(torch.FloatTensor)
+    if initial_checkpoint is not None:
+        torch.load(initial_checkpoint, map_location="cpu")
     print(f"Loaded model and tokenizer in {time.time() - start_time:.2f} seconds")
     return model, tokenizer
--- src/main/util/__init__.py
+++ src/main/util/__init__.py
@@ -1,2 +1,3 @@
 from src.main.util.data import get_pile_dataloaders, load_pile_dataset
 from src.main.util.metrics import compute_loss, accuracy
+from src.main.util.checkpoints import save_checkpoint, load_checkpoint, generate_checkpoint_name
